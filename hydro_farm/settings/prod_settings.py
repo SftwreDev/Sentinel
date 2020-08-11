29850d8923a129bc8a5d5d4976b51ad9d3d7e968
Gian Carlo Llanes Garcia <49069702+SftwreDev@users.noreply.github.com>
2020-08-11 20:44:56 +0800

    Update prod_settings.py

--- hydro_farm/settings/prod_settings.py
+++ hydro_farm/settings/prod_settings.py
@@ -3,6 +3,6 @@ from .common_settings import BASE_DIR
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['hydrofarm.herokuapp.com']
+ALLOWED_HOSTS = ['hydrofarms-sentinel.herokuapp.com']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
